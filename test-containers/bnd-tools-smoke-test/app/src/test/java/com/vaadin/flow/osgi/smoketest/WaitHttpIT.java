ab28f933ef687eaf9389cbf516dd3f1942b85f2b
Denis Anisimov <denis@vaadin.com>
2021-01-11 14:30:59 +0300

    fix: make jetty listen all ips, correct IT

--- test-containers/bnd-tools-smoke-test/app/src/test/java/com/vaadin/flow/osgi/smoketest/WaitHttpIT.java
+++ test-containers/bnd-tools-smoke-test/app/src/test/java/com/vaadin/flow/osgi/smoketest/WaitHttpIT.java
@@ -1,46 +1,28 @@
-/*
- * Copyright 2000-2020 Vaadin Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package com.vaadin.flow.osgi.smoketest;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 
-import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 import com.vaadin.flow.testutil.ChromeBrowserTest;
 
-public class SmokeIT extends ChromeBrowserTest {
+public class WaitHttpIT extends ChromeBrowserTest {
 
     @Override
     public void checkIfServerAvailable() {
     }
 
     @Override
-    protected String getTestPath() {
-        return "/smoke-view";
+    public void setup() {
+
     }
 
     @Test
-    public void buttonIsShown_clickIsHandled()
+    public void waitForHttp()
             throws MalformedURLException, InterruptedException {
+        // This is not really a test.
         // It allows to wait when HTTP container inside OSGi becomes ready.
         // Without this workaround IT tests starts immediately because there is
         // no maven server plugin which runs the server and wait when it becomes
@@ -49,19 +31,11 @@ public class SmokeIT extends ChromeBrowserTest {
         // separate JVM and no one waits for its readiness.
         // As a result IT tests starts immediately and this workaround is used
         // to wait when HTTP server starts to handle HTTP requests.
-        waitViewUrl(60);
-
-        super.checkIfServerAvailable();
-
-        open();
-
-        WebElement action = findElement(By.id("action"));
-        action.click();
-
-        Assert.assertTrue(isElementPresent(By.id("info")));
+        // It's executed before any other IT test.
+        waitHttp(60);
     }
 
-    private void waitViewUrl(int count)
+    private void waitHttp(int count)
             throws MalformedURLException, InterruptedException {
         String rootUrl = getRootURL();
         if (count == 0) {
@@ -70,11 +44,11 @@ public class SmokeIT extends ChromeBrowserTest {
         }
         URL url = new URL(rootUrl);
         try {
-            URLConnection connection = url.openConnection();
-            connection.connect();
+            url.openConnection();
         } catch (IOException exception) {
             Thread.sleep(1000);
-            waitViewUrl(count - 1);
+            waitHttp(count - 1);
         }
     }
+
 }

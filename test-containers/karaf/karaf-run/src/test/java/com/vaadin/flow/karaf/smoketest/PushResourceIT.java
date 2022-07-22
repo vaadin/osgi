bbbc1c7ea2090e1e619ea0c19fd84cf9405619ce
marcin <106965834+MarcinVaadin@users.noreply.github.com>
2022-07-22 10:47:09 +0200

    test: fixes flaky tests due to asynchronous karaf startup (#105) (#106)

--- test-containers/karaf/karaf-run/src/test/java/com/vaadin/flow/karaf/smoketest/PushResourceIT.java
+++ test-containers/karaf/karaf-run/src/test/java/com/vaadin/flow/karaf/smoketest/PushResourceIT.java
@@ -2,29 +2,20 @@ package com.vaadin.flow.karaf.smoketest;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
 
 import org.apache.commons.io.IOUtils;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
-import org.junit.Assert;
 import org.junit.Test;
 
-import com.vaadin.flow.testutil.ChromeBrowserTest;
-
-public class PushResourceIT extends ChromeBrowserTest {
+public class PushResourceIT extends BaseKarafTest {
 
     @Test
     public void pushResourceIsRegistered() throws IOException {
-        String rootUrl = getRootURL();
-        String pushPath = rootUrl + "/VAADIN/static/push/vaadinPush.js";
-
-        URL url = new URL(pushPath);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        Assert.assertEquals(HttpURLConnection.HTTP_OK,
-                connection.getResponseCode());
+        HttpURLConnection connection = waitUntilHttpOk(getRootURL() +
+                "/VAADIN/static/push/vaadinPush.js", 180);
         String content = IOUtils
                 .readLines(connection.getInputStream(), StandardCharsets.UTF_8)
                 .stream().collect(Collectors.joining("\n"));
